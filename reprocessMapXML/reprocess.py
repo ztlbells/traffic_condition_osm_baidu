443fbc529facaed23012eee5c0908e786ec30862
ztlbells <zutianluo@gmail.com>
2016-11-12 11:52:49 +0800

    calculate distance

--- reprocessMapXML/reprocess.py
+++ reprocessMapXML/reprocess.py
@@ -4,10 +4,11 @@
 # sys argvs
 ways_xml                    = './xml/output.xml' #sys.argv[1]
 map_xml         			= './xml/map.xml' #sys.argv[2]
-output_csv_xml          	= 'ways_lat_lon.csv'#sys.argv[3]
+output_csv_xml          	= 'ways_lat_lon_dis.csv'#sys.argv[3]
 
 import xmltodict
 import csv 
+import math
 
 def node_info(node_id, node_list) :
 	match = 0
@@ -17,6 +18,24 @@ def node_info(node_id, node_list) :
 			return (node['@lat'], node['@lon'])
 	if match == 0:
 		return (0.0, 0.0)
+
+def getDistance(srt_pnt, end_pnt):
+        #radius of Earth
+        R = 6378137 
+        PI = math.pi
+        srt_pnt_lat = float(srt_pnt[0]) * PI / 180.0 
+        end_pnt_lat = float(end_pnt[0]) * PI / 180.0 
+
+        lat_dif = srt_pnt_lat - end_pnt_lat;  
+        lon_dif = (float(srt_pnt[1]) - float(end_pnt[1])) * PI / 180.0 
+
+        sin_lat = math.sin(lat_dif / 2.0);  
+        sin_lon = math.sin(lon_dif / 2.0);  
+
+        distance = 2 * R  * math.asin(\
+        					math.sqrt(sin_lat * sin_lat + math.cos(lat_dif) * math.cos(lon_dif) * (sin_lon**2))\
+        							)
+        return distance
 
 # read ways
 ways_doc = open(ways_xml,'r')
@@ -41,7 +60,7 @@ print 'number of ways:', len_of_ways, \
 csv_file = file (output_csv_xml,"w")
 writer = csv.writer(csv_file, quoting = csv.QUOTE_ALL)
 writer.writerow(['way_id', 'srt_pnt_lat', 'srt_pnt_lon', \
-							'end_pnt_lat', 'end_pnt_lat'])
+							'end_pnt_lat', 'end_pnt_lon', 'distance'])
 
 for way in ways :
 	nds = len(way['nd'])
@@ -49,8 +68,18 @@ for way in ways :
 	start_nd = way['nd'][0]['@ref']
 	end_nd = way['nd'][nds - 1]['@ref']
 
-	writer.writerow([way['@id'], node_info(start_nd, nodes)[0], node_info(start_nd, nodes)[1],\
-								node_info(end_nd, nodes)[0], node_info(end_nd, nodes)[1] ])
+	start_nd_info = node_info(start_nd, nodes)
+	end_nd_info	= node_info(end_nd, nodes)
+
+	total_distance = 0
+	for counter in range(0, len(way['nd']) - 1) :
+		start_nd = way['nd'][counter]['@ref']
+		end_nd   = way['nd'][counter + 1]['@ref']
+		total_distance += getDistance(node_info(start_nd, nodes),node_info(end_nd, nodes))
+
+	writer.writerow([way['@id'], start_nd_info[0], start_nd_info[1],\
+								end_nd_info[0], end_nd_info[1], \
+								str(total_distance) ])
 
 csv_file.close()
 
