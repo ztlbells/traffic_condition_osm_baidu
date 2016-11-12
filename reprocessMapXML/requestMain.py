fd944b401e8c96ffb231a5163e3546ee59afde89
ztlbells <zutianluo@gmail.com>
2016-11-12 12:37:31 +0800

    Add files via upload

--- reprocessMapXML/requestMain.py
+++ reprocessMapXML/requestMain.py
@@ -10,7 +10,7 @@ import time
 #developer key
 ak 						= '&ak=tzWqf1kD8Gd6l6tGm9rOyoVs5Q4jXQoZ'
 
-csv_file_path      		= './ways_lat_lon.csv' #sys.argv[1]
+csv_file_path      		= './ways_lat_lon_dis.csv' #sys.argv[1]
 output_csv 				= './output_' + str(time.localtime()) + '.csv'
 start_time				= time.time()
 
@@ -33,15 +33,13 @@ def attain_baidu_coords(lat, lon):
 	return hjson['result']
 
 
-
-
 ########### request traffic condition part #######################
 # request example 
 # http://api.map.baidu.com/direction/v1?mode=driving&origin=39.921673,116.484644&destination=39.9231,
 # 116.484669&origin_region=%E5%8C%97%E4%BA%AC&destination_region=%E5%8C%97%E4%BA%AC&ak=
 # tzWqf1kD8Gd6l6tGm9rOyoVs5Q4jXQoZ
 
-def request_baidu_dir(srt_pnt_lat, srt_pnt_lon, end_pnt_lat, end_pnt_lon):
+def request_baidu_dir(srt_pnt_lat, srt_pnt_lon, end_pnt_lat, end_pnt_lon, org_distance):
 	url 				= 'http://api.map.baidu.com/direction/v1?'
 	mode 				= 'mode=driving'
 
@@ -64,11 +62,18 @@ def request_baidu_dir(srt_pnt_lat, srt_pnt_lon, end_pnt_lat, end_pnt_lon):
 	hjson = json.loads(temp.read())
 
 	traffic_condition 	= hjson['result']['traffic_condition']
+	steps 				= hjson['result']['routes'][0]['steps'][0]
 	distance			= hjson['result']['routes'][0]['distance']
 	duration			= hjson['result']['routes'][0]['duration']
 
-	#print 'cond', traffic_condition, 'dis', distance, 'dur', duration
-	return (traffic_condition, distance, duration)
+	if len(steps) > 1:
+		# if the number of 'content' > 1, we need the average value of duration / distance
+		ratio			= float(duration) / float(distance)
+		duration 		= float(org_distance) * ratio
+		return (traffic_condition, org_distance, str(duration))
+	else :
+		#print 'cond', traffic_condition, 'dis', distance, 'dur', duration
+		return (traffic_condition, distance, duration)
 
 ########### reading csv part ##################
 ways_csv_file = file (csv_file_path,"r")
@@ -81,13 +86,9 @@ counter = 0
 for line in reader :
 
     if line[0] != 'way_id':
-    	srt_pnt_lat = line[1]
-    	srt_pnt_lon = line[2]
-    	end_pnt_lat = line[3]
-    	end_pnt_lon = line[4]
 
     	#print line
-    	result = request_baidu_dir(srt_pnt_lat, srt_pnt_lon, end_pnt_lat, end_pnt_lon)
+    	result = request_baidu_dir(line[1], line[2], line[3], line[4], line[5])
     	writer.writerow([line[0], result[0], result[1], result[2]])
     	counter += 1
     	print counter, "running time:", time.time() - start_time
